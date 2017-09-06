f4fa60c4ab64e511841561dbfecf6b16ab81dc7e
ZZFM <flyingman_pan@outlook.com>
2017-09-06 09:34:12 +0800

    autoform framework

---
+++ magic_square.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+bool judge();
+bool make();
+int main()
+{
+	int status;
+	cout<< "Judge(1) or make(2)"<<endl;
+	cin>>status;
+	if(status==1)
+		if(judge())
+			cout<< "true\n";
+		else
+			cout<< "false\n";
+	else
+		make();
+}
+bool judge()
+{
+	int size;
+	cout<< "Input>";
+	cin>>size;
+	int data[size][size];
+	for(int i=0;i<size;i++)
+	{
+		for(int j=0;j<size;j++)
+		{
+			cin>>data[i][j];
+		}
+	}
+	int basic=0;
+	for(int i=0;i<size;i++)
+	{
+		basic+=data[0][i];
+	}
+	//cout<< "\n"<<basic<< "\n";
+	
+	for(int i=0;i<size;i++)
+	{
+		int temp=0;
+		for(int j=0;j<size;j++)
+		{
+			temp+=data[i][j];
+		}
+		if(temp!=basic)
+		{
+			//cout<<"first"<<i<<endl;
+			return false;
+		}
+			
+	}
+	for(int i=0;i<size;i++)
+	{
+		int temp=0;
+		for(int j=0;j<size;j++)
+		{
+			temp+=data[j][i];
+		}
+		if(temp!=basic)
+		{
+			//cout<< "second"<<i<<endl;
+			return false;
+		}
+	}
+	
+	int temp1=0,temp2=0;
+	for(int i=0;i<size;i++)
+	{
+		temp1+=data[i][i];
+		temp2+=data[i][size-i-1];
+	}
+	if(temp1!=basic||temp2!=basic)
+	{
+		//cout<< "third"<<temp1<< "   "<<temp2<<endl;
+		return false;
+	}
+	return true;
+}
+bool make()
+{
+	;
+}
